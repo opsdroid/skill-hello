9e00c1844195b32601872f1ca4ed00368f9ef6c7
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-11-08 18:31:47 +0000

    Testing for rasa

--- __init__.py
+++ __init__.py
@@ -1,16 +1,34 @@
-from opsdroid.matchers import match_regex
+from opsdroid.matchers import match_regex, match_rasanlu_intent
 import logging
 import random
+
 
 def setup(opsdroid):
     logging.debug("Loaded hello module")
 
+
 @match_regex(r'hi|hello|hey|hallo')
+@match_rasanlu_intent('smalltalk.hello')
 async def hello(opsdroid, config, message):
     text = random.choice(["Hi {}", "Hello {}", "Hey {}"]).format(message.user)
     await message.respond(text)
 
+
 @match_regex(r'bye( bye)?|see y(a|ou)|au revoir|gtg|I(\')?m off')
+@match_rasanlu_intent('smalltalk.goodbye')
 async def goodbye(opsdroid, config, message):
     text = random.choice(["Bye {}", "See you {}", "Au revoir {}"]).format(message.user)
     await message.respond(text)
+
+
+@match_rasanlu_intent('smalltalk.how-are-you')
+async def how_are_you(opsdroid, config, message):
+    text = random.choice(["Good thanks", "Ok thanks", "Fine thanks"]).format(message.user)
+    await message.respond(text)
+
+
+@match_rasanlu_intent('smalltalk.name')
+async def how_are_you(opsdroid, config, message):
+    text = random.choice(["I'm opsdroid", "My name is opsdroid", "It's opsdroid"]).format(message.user)
+    await message.respond(text)
+
---
+++ intents.md
@@ -0,0 +1,45 @@
+## intent:smalltalk.hello
+- Hey
+- Hello
+- Hi
+- Hiya
+- hey
+- whats up
+- wazzup
+- heya
+- salut
+- bonjour
+- hola
+- heeey
+- heyyy
+- hallo
+- howdy
+
+## intent:smalltalk.goodbye
+- bye
+- cya
+- see you later
+- I'm off, see you later
+- Goodbye
+- see you tomorrow
+- catch you later
+- Speak to you next week
+
+## intent:smalltalk.how-are-you
+- alright?
+- how are you?
+- hows it going?
+- you ok?
+- hey, how are you?
+- how are you feeling?
+- you alright?
+- how are you my friend?
+- hey you ok
+- how are you
+
+## intent:smalltalk.name
+- whats your name?
+- what are you called?
+- what should I call you?
+- how should I address you?
+- Who are you?
