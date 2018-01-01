2c8a69a74a95e30d28bc394dbc3ff008b093ee86
Jacob Tomlinson <jacob@tom.linson.uk>
2018-01-01 16:03:01 +0000

    Correct matcher name

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from opsdroid.matchers import match_regex, match_rasanlu_intent
+from opsdroid.matchers import match_regex, match_rasanlu
 import logging
 import random
 
@@ -8,26 +8,26 @@ def setup(opsdroid):
 
 
 @match_regex(r'hi|hello|hey|hallo')
-@match_rasanlu_intent('smalltalk.hello')
+@match_rasanlu('smalltalk.hello')
 async def hello(opsdroid, config, message):
     text = random.choice(["Hi {}", "Hello {}", "Hey {}"]).format(message.user)
     await message.respond(text)
 
 
 @match_regex(r'bye( bye)?|see y(a|ou)|au revoir|gtg|I(\')?m off')
-@match_rasanlu_intent('smalltalk.goodbye')
+@match_rasanlu('smalltalk.goodbye')
 async def goodbye(opsdroid, config, message):
     text = random.choice(["Bye {}", "See you {}", "Au revoir {}"]).format(message.user)
     await message.respond(text)
 
 
-@match_rasanlu_intent('smalltalk.how-are-you')
+@match_rasanlu('smalltalk.how-are-you')
 async def how_are_you(opsdroid, config, message):
     text = random.choice(["Good thanks", "Ok thanks", "Fine thanks"]).format(message.user)
     await message.respond(text)
 
 
-@match_rasanlu_intent('smalltalk.name')
+@match_rasanlu('smalltalk.name')
 async def how_are_you(opsdroid, config, message):
     text = random.choice(["I'm opsdroid", "My name is opsdroid", "It's opsdroid"]).format(message.user)
     await message.respond(text)
