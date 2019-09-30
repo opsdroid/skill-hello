f68938894cb90a28cef1d9e461f668f589fcb4fc
Martin Musale <martinmshale@gmail.com>
2019-10-01 02:16:15 +0300

    Change from function-based to class-based skills

--- __init__.py
+++ __init__.py
@@ -1,14 +1,19 @@
-from opsdroid.matchers import match_regex
-import logging
 import random
 
+from opsdroid.matchers import match_regex
+from opsdroid.skill import Skill
 
-@match_regex(r'hi|hello|hey|hallo')
-async def hello(opsdroid, config, message):
-    text = random.choice(["Hi {}", "Hello {}", "Hey {}"]).format(message.user)
-    await message.respond(text)
 
-@match_regex(r'bye( bye)?|see y(a|ou)|au revoir|gtg|I(\')?m off')
-async def goodbye(opsdroid, config, message):
-    text = random.choice(["Bye {}", "See you {}", "Au revoir {}"]).format(message.user)
-    await message.respond(text)
+class HelloByeSkill(Skill):
+
+    @match_regex(r'hi|hello|hey|hallo')
+    async def hello(self, message):
+        text = random.choice(
+            ["Hi {}", "Hello {}", "Hey {}"]).format(message.user)
+        await message.respond(text)
+
+    @match_regex(r'bye( bye)?|see y(a|ou)|au revoir|gtg|I(\')?m off')
+    async def goodbye(self, message):
+        text = random.choice(
+            ["Bye {}", "See you {}", "Au revoir {}"]).format(message.user)
+        await message.respond(text)
